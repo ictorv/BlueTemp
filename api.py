786492ffef3908ad04e51ef65e1357d624d7fc00
S Victor Kumar <victor.mygit@gmail.com>
2026-05-27 21:31:53 +0530

    delete: middleware
    Removed CORS middleware configuration from the API.

--- api.py
+++ api.py
@@ -34,13 +34,6 @@ app = FastAPI(
     version="1.1.0",
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 def _http_error(exc: GitHubError):
